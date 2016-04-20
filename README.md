924e969019a69919f67adc21b975b402c64e8002
Renaud <renaud.hebert-legault@crim.ca>
2016-04-20 15:16:53 -0400

    added recharts example

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## React Starter Kit + Recharts Server-side
-[![Support us on Bountysource](https://dl.dropboxusercontent.com/u/16006521/react-starter-kit/banner.png)](https://salt.bountysource.com/teams/react-starter-kit)<br>
 
 > [React Starter Kit](https://www.reactstarterkit.com) is an opinionated
 > boilerplate for web development built on top of Facebook's
@@ -11,15 +10,6 @@
 > Helping you to stay productive following the best practices. A solid starting
 > point for both professionals and newcomers to the industry.
 
-See [demo](http://demo.reactstarterkit.com) &nbsp;|&nbsp;
-[docs](https://github.com/kriasoft/react-starter-kit/tree/master/docs) &nbsp;|&nbsp;
-[to-do list](https://waffle.io/kriasoft/react-starter-kit) &nbsp;|&nbsp;
-join [#react-starter-kit](https://gitter.im/kriasoft/react-starter-kit) chatroom to stay up to date &nbsp;|&nbsp;
-visit our sponsors:
-
-[![Rollbar - Full-stack error tracking for all apps in any language](https://dl.dropboxusercontent.com/u/16006521/react-starter-kit/rollbar.png)](https://rollbar.com/?utm_source=reactstartkit(github)&utm_medium=link&utm_campaign=reactstartkit(github)) &nbsp;&nbsp;
-[![Localize - Translate your web app in minutes](https://dl.dropboxusercontent.com/u/16006521/react-starter-kit/localize.png)](https://localizejs.com/?cid=802&utm_source=rsk)
-
 ### Prerequisites
 
   "node": ">=5.0 <6",
@@ -28,7 +18,6 @@ visit our sponsors:
 ### How to execute
 
 ```
-.
 npm install
 npm start
 ```
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,36 +8,32 @@
  */
 
 import React, { PropTypes } from 'react';
+import Recharts, {ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.scss';
 
-function Home({ news }) {
+const data = [{name: 'Page A', uv: 590, pv: 800, amt: 1400},
+  {name: 'Page B', uv: 868, pv: 967, amt: 1506},
+  {name: 'Page C', uv: 1397, pv: 1098, amt: 989},
+  {name: 'Page D', uv: 1480, pv: 1200, amt: 1228},
+  {name: 'Page E', uv: 1520, pv: 1108, amt: 1100},
+  {name: 'Page F', uv: 1400, pv: 680, amt: 1700}];
+
+function Home() {
+  console.log("Render ComposedChart");
   return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1 className={s.title}>React.js News</h1>
-        <ul className={s.news}>
-          {news.map((item, index) => (
-            <li key={index} className={s.newsItem}>
-              <a href={item.link} className={s.newsTitle}>{item.title}</a>
-              <span
-                className={s.newsDesc}
-                dangerouslySetInnerHTML={{ __html: item.contentSnippet }}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <ComposedChart width={600} height={400} data={data}
+                   margin={{top: 20, right: 80, bottom: 20, left: 20}}>
+      <XAxis dataKey="name" label="Pages"/>
+      <YAxis label="Index"/>
+      <Tooltip/>
+      <Legend/>
+      <CartesianGrid stroke='#f5f5f5'/>
+      <Area type='monotone' dataKey='amt' fill='#8884d8' stroke='#8884d8'/>
+      <Bar dataKey='pv' barSize={20} fill='#413ea0'/>
+      <Line type='monotone' dataKey='uv' stroke='#ff7300'/>
+    </ComposedChart>
   );
 }
-
-Home.propTypes = {
-  news: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    contentSnippet: PropTypes.string,
-  })).isRequired,
-};
 
 export default withStyles(Home, s);
